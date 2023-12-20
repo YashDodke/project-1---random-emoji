6019aba5ae2be5a15f215dd669ff3cfd5455a374
YashDodke <yashdodke1234@gmail.com>
2023-12-20 22:55:03 +0530

    html css and js updated

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const emoji = document.querySelector("#emoji");
-
+const h2 = document.querySelector(".h2");
 const emojis = [
   "😆",
   "😅",
@@ -36,6 +36,10 @@ const emojis = [
   "😁",
   "😬",
 ];
+const texts =["hehe","told ya","u understand now?","ask your girl","your girl got mood swings too","keep hovering","u like this emoji?","this emoji is exactly her"];
 emoji.addEventListener("mouseover",()=>{
-    emoji.innerHTML= emojis[Math.floor(Math.random()*emojis.length)]
+    // h2.innerHTML=texts[math.floor(Math.random()*texts.length)];
+    emoji.innerHTML= emojis[Math.floor(Math.random()*emojis.length)];
 })
+
+
